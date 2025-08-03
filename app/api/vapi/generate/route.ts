c85d675fc3d3a6f4acc1da98fb034054d18de24f
vinsmokejazz <vinsmokejazz@gmail.com>
2025-08-04 00:17:04 +0530

    sm

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -5,6 +5,10 @@ import { getRandomInterviewCover } from "@/lib/utils";
 import { db } from "@/firebase/admin";
 
 
+export async function GET() {
+  return Response.json({ success: true,data:'THANK YOU' }, { status: 200 });
+}
+
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
   try {
