b645a1b913ae953c20a7551b253a2efa83d0b7d1
vinsmokejazz <vinsmokejazz@gmail.com>
2025-08-03 18:36:58 +0530

    componet upd

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h3>Interview Generation</h3>
+    <Agent/>
+    </>
+  )
+}
+
+export default page
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import InterviewCard from '../components/InterviewCard'
+import InterviewCard from '../../components/InterviewCard'
 
 const Homepage = () => {
   return (
